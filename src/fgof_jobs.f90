ca7f4a676a3508b888709a550d4eb8c21cf27386
mfwolffe <wolffemf@dukes.jmu.edu>
2026-05-03 17:02:38 -0400

    Build jobs lifecycle

--- src/fgof_jobs.f90
+++ src/fgof_jobs.f90
@@ -7,13 +7,22 @@ module fgof_jobs
   character(len=*), parameter :: FGOF_JOBS_BACKEND_POSIX = "posix"
 
   public :: &
+    attach_job, &
     clear_job_handle, &
     clear_job_result, &
     clear_job_spec, &
+    complete_job, &
+    configure_job, &
     job_handle, &
+    job_is_configured, &
+    job_is_finished, &
+    job_is_running, &
+    job_needs_cleanup, &
     job_result, &
     job_spec, &
-    jobs_backend_name
+    jobs_backend_name, &
+    make_job_spec, &
+    release_job
 
 contains
 
@@ -27,10 +36,16 @@ contains
   function clear_job_handle() result(handle)
     type(job_handle) :: handle
 
+    handle%spec = clear_job_spec()
+    handle%result = clear_job_result()
     handle%pid = 0
     handle%process_group = 0
+    handle%configured = .false.
     handle%running = .false.
+    handle%finished = .false.
     handle%background = .false.
+    handle%owns_process = .false.
+    handle%cleanup_needed = .false.
   end function clear_job_handle
 
   function clear_job_result() result(result_value)
@@ -41,7 +56,108 @@ contains
     result_value%exited = .false.
     result_value%signaled = .false.
     result_value%stopped = .false.
+    result_value%available = .false.
   end function clear_job_result
+
+  function make_job_spec(command, argv, background, new_process_group) result(spec)
+    character(len=*), intent(in) :: command
+    character(len=*), intent(in), optional :: argv(:)
+    logical, intent(in), optional :: background
+    logical, intent(in), optional :: new_process_group
+    type(job_spec) :: spec
+
+    spec = clear_job_spec()
+
+    if (len_trim(command) > 0) spec%command = trim(command)
+    if (present(argv)) spec%argv = argv
+    if (present(background)) spec%background = background
+    if (present(new_process_group)) spec%new_process_group = new_process_group
+  end function make_job_spec
+
+  subroutine configure_job(handle, spec)
+    type(job_handle), intent(inout) :: handle
+    type(job_spec), intent(in) :: spec
+
+    handle = clear_job_handle()
+    handle%spec = spec
+    handle%configured = allocated(spec%command)
+    handle%background = spec%background
+  end subroutine configure_job
+
+  subroutine attach_job(handle, pid, process_group, owns_process)
+    type(job_handle), intent(inout) :: handle
+    integer, intent(in) :: pid
+    integer, intent(in), optional :: process_group
+    logical, intent(in), optional :: owns_process
+
+    if (.not. handle%configured) return
+    if (pid <= 0) return
+
+    handle%pid = pid
+    if (present(process_group)) then
+      if (process_group > 0) then
+        handle%process_group = process_group
+      else
+        handle%process_group = pid
+      end if
+    else
+      handle%process_group = pid
+    end if
+
+    if (present(owns_process)) then
+      handle%owns_process = owns_process
+    else
+      handle%owns_process = .true.
+    end if
+
+    handle%running = .true.
+    handle%finished = .false.
+    handle%cleanup_needed = handle%owns_process
+    handle%result = clear_job_result()
+  end subroutine attach_job
+
+  subroutine complete_job(handle, result_value)
+    type(job_handle), intent(inout) :: handle
+    type(job_result), intent(in) :: result_value
+
+    handle%result = result_value
+    handle%result%available = result_value%available .or. result_value%exited .or. &
+                              result_value%signaled .or. result_value%stopped
+    handle%running = .false.
+    handle%finished = handle%result%available
+    handle%cleanup_needed = .false.
+  end subroutine complete_job
+
+  subroutine release_job(handle)
+    type(job_handle), intent(inout) :: handle
+
+    handle%owns_process = .false.
+    handle%cleanup_needed = .false.
+  end subroutine release_job
+
+  logical function job_is_configured(handle) result(configured)
+    type(job_handle), intent(in) :: handle
+
+    configured = handle%configured
+  end function job_is_configured
+
+  logical function job_is_running(handle) result(running)
+    type(job_handle), intent(in) :: handle
+
+    running = handle%configured .and. handle%running
+  end function job_is_running
+
+  logical function job_is_finished(handle) result(finished)
+    type(job_handle), intent(in) :: handle
+
+    finished = handle%finished
+  end function job_is_finished
+
+  logical function job_needs_cleanup(handle) result(needs_cleanup)
+    type(job_handle), intent(in) :: handle
+
+    needs_cleanup = handle%cleanup_needed
+  end function job_needs_cleanup
 
   function jobs_backend_name() result(name)
     character(len=:), allocatable :: name
--- src/fgof_jobs_types.f90
+++ src/fgof_jobs_types.f90
@@ -9,19 +9,26 @@ module fgof_jobs_types
     logical :: new_process_group = .true.
   end type job_spec
 
-  type, public :: job_handle
-    integer :: pid = 0
-    integer :: process_group = 0
-    logical :: running = .false.
-    logical :: background = .false.
-  end type job_handle
-
   type, public :: job_result
     integer :: exit_code = 0
     integer :: signal = 0
     logical :: exited = .false.
     logical :: signaled = .false.
     logical :: stopped = .false.
+    logical :: available = .false.
   end type job_result
+
+  type, public :: job_handle
+    type(job_spec) :: spec
+    type(job_result) :: result
+    integer :: pid = 0
+    integer :: process_group = 0
+    logical :: configured = .false.
+    logical :: running = .false.
+    logical :: finished = .false.
+    logical :: background = .false.
+    logical :: owns_process = .false.
+    logical :: cleanup_needed = .false.
+  end type job_handle
 
 end module fgof_jobs_types
